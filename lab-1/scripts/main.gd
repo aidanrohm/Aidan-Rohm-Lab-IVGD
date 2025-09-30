041086ca7f52335766401206869394eb05667b45
aidanrohm <aidanrohm@gmail.com>
2025-09-29 20:24:56 -0400

    Bug fixes and completion of assignment
    Fixed some code bugs regarding the way Collectibles was being accessed

--- lab-1/scripts/main.gd
+++ lab-1/scripts/main.gd
@@ -1,20 +1,27 @@
 extends Node2D
 
 @onready var _score_label = $CanvasLayer/ScoreLabel
+@onready var _collectibles = $Collectibles.get_children()
+
 var score: int = 0
+var remaining_collectibles: int = 0
 
 func _ready() -> void:
-	for collectible in $Collectibles.get_children():
+	remaining_collectibles = _collectibles.size()
+
+	for collectible in _collectibles:
 		if collectible.has_signal("collected"):
 			collectible.connect("collected", Callable(self, "_on_collectible_collected"))
-			
-	_update_score_label()
 	
+	_update_score_label()
+
 func _on_collectible_collected(value: int) -> void:
 	score += value
-	_update_score_label()
-	if $Collectible.get_child_count() == 0:
-		_score_label.text = "All collected! %d" % score
-		
+	remaining_collectibles -= 1
+	if remaining_collectibles <= 0:
+		_score_label.text = "All collected!"
+	else:
+		_update_score_label()
+
 func _update_score_label() -> void:
 	_score_label.text = "Score: %d" % score
